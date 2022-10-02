42d70b9e07c7d88a76d138d82bb87ad946ed246c
yusef <yusefdev@outlook.fr>
2022-10-02 14:19:13 +0200

    ajout de la validation 1 a 10 pour HEAT

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -7,7 +7,12 @@ const modelSauce = mongoose.Schema({
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  heat: { type: Number, required: true },
+  heat: { 
+    type: Number, 
+    required: true,
+    min: [1, 'NE peut pas être inferieur à 1'],
+    max: [10, 'ne peut pas être supérieur à 10']
+  },
   imageUrl: { type: String, required: true },
   mainPepper: { type: String, required: true },
   usersLiked: { type: Array },
